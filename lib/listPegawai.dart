17592aaf98703ddd65601a156cca48b7289d06da
gugunm <mediamergugun2@gmail.com>
2021-05-19 20:31:26 +0700

    Berhasil fetch data pegawai

--- lib/listPegawai.dart
+++ lib/listPegawai.dart
@@ -5,32 +5,28 @@ import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'model/pegawai.dart';
 
-class HttpService {
-  Future<List<Pegawai>> fetchPegawai() async {
-    await Future.delayed(Duration(seconds: 1));
+Future<List<Pegawai>> fetchPegawai() async {
+  await Future.delayed(Duration(seconds: 1));
 
-    final res =
-        await http.get(Uri.http('app.bpkp.go.id', 'apilatihan/pegawai.php'));
+  final res =
+      await http.get(Uri.http('app.bpkp.go.id', 'apilatihan/pegawai.php'));
 
-    if (res.statusCode == 200) {
-      List<dynamic> body = jsonDecode(res.body);
+  if (res.statusCode == 200) {
+    List<dynamic> body = jsonDecode(res.body);
 
-      List<Pegawai> pegawai = body
-          .map(
-            (dynamic item) => Pegawai.fromJson(item),
-          )
-          .toList();
+    List<Pegawai> pegawai = body
+        .map(
+          (dynamic item) => Pegawai.fromJson(item),
+        )
+        .toList();
 
-      return pegawai;
-    } else {
-      throw "Unable to retrieve data.";
-    }
+    return pegawai;
+  } else {
+    throw "Unable to retrieve data.";
   }
 }
 
 class ListPegawai extends StatelessWidget {
-  final HttpService httpService = HttpService();
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -38,7 +34,7 @@ class ListPegawai extends StatelessWidget {
         title: Text("Posts"),
       ),
       body: FutureBuilder(
-        future: httpService.fetchPegawai(),
+        future: fetchPegawai(),
         builder: (BuildContext context, AsyncSnapshot<List<Pegawai>> snapshot) {
           if (snapshot.hasData) {
             List<Pegawai> pegawai = snapshot.data!;
